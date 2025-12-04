90d5ecee1ff199bf07542d9538d8c63baf7cc4b8
vineetsitm09-debug <vineetsitm09@gmail.com>
2025-12-04 18:01:51 +0530

    Update types.ts

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+export type VideoItem = {
+  id: string;
+  url: string;
+  title: string;
+  filename?: string;
+  thumbnail?: string;
+  uploader?: string;
+  created_at?: string;
+  status?: string;
+};
