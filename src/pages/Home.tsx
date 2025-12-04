7108382d9081e39d2ef220fe8e62f4f1e5d128c8
vineetsitm09-debug <vineetsitm09@gmail.com>
2025-12-04 17:08:26 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="p-6 text-white">
-      <h1 className="text-2xl font-bold mb-4">🏠 Home</h1>
+      <h1 className="text-2xl font-bold mb-4">🏠 ssHome</h1>
       <p>Welcome to <span className="font-bold text-red-500">StreamLite</span>.  
          Select a video from your library or upload a new one to start watching.</p>
     </div>
